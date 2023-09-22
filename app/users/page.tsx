f19cffcad11de7d9f9e3322d831371adeb022e99
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-09-22 18:48:18 +0530

    server side components and client side components

---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductCard from '../components/ProductCard';
+
+const UsersPage = () => {
+  return (
+	<div>UsersPage
+		<ProductCard/>
+	</div>
+  )
+}
+
+export default UsersPage;
