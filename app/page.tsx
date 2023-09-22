718c44a8530197da96e84ccf1df2c70d6c557ce5
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-09-22 21:34:05 +0530

    server-side-dynamic-rendering At request time

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import UsersPage from './users/page'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
-      <UsersPage/>
+      <Link href="/users" >Users</Link>
     </main>
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ProductCard from '../components/ProductCard';
+'use client'
 
-const UsersPage = () => {
-  return (
-	<div>UsersPage
-		<ProductCard/>
-	</div>
-  )
-}
+import React from "react";
+
+const UsersPage = async () => {
+
+	interface User {
+		id: number;
+		name: string;
+	}
+
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const users: User[] = await res.json();
+
+	return (
+		<>
+			<h1>UsersPage</h1>
+			<ul>
+				{users?.map( user => <li key={user?.id} >{user?.name}</li>)}
+			</ul>
+		</>
+	);
+};
 
 export default UsersPage;
